2a271b079bf5bc826aa846dd417d195a6c27e9a6
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-02-04 04:05:29 +0000

    [Add] 3-8

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -1,13 +1,27 @@
 class BooksController < ApplicationController
-  def index
-  end
 
   def show
   end
 
+  def index
+  end
+
   def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
   end
 
   def edit
   end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- Bookers/config/routes.rb
+++ Bookers/config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/edit'
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
