0a996a8ce8bc916ca5f4f9e62da04d20f974e777
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-02-04 14:53:45 +0000

    [Add] 3-8

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -18,10 +18,23 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path(book.id)
+    redirect_to book_path(book.id)
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
   private
--- Bookers/config/routes.rb
+++ Bookers/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: 'books#top'
   get 'books' => 'books#index'
   post 'books' => 'books#create'
-  get 'books/:id' => 'books#show'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as:'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
