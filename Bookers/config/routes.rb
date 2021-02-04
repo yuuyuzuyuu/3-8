f3f60196f6821dc6162b7779b36ef7a9f776212d
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-02-04 00:25:01 +0000

    [Start] 3-8

---
+++ Bookers/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/edit'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
